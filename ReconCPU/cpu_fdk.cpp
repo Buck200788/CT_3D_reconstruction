4a1721d21a432bcfb44ed0ddf8aba4019b985c2e
Buck200788 <liangwuyangbuck@gmail.com>
2026-07-22 19:11:03 +0800

    delete the BackProjectOMP content so as to make sure the CPU can run well

--- ReconCPU/cpu_fdk.cpp
+++ ReconCPU/cpu_fdk.cpp
@@ -11,23 +11,5 @@ void CpuFDKRecon::Reconstruct(const std::vector<float>& proj, std::vector<float>
 
 void CpuFDKRecon::BackProjectOMP(const std::vector<float>& proj, std::vector<float>& vol)
 {
-    const auto& g = m_geo;
-#pragma omp parallel
-    {
-        int tid = omp_get_thread_num();
-        int thr = omp_get_num_threads();
-        int zblk = g.nz / thr;
-        int zs = tid * zblk;
-        int ze = (tid == thr - 1) ? g.nz : (tid + 1) * zblk;
-        for (int z = zs; z < ze; z++)
-        {
-            for (int y = 0; y < g.ny; y++)
-            {
-                for (int x = 0; x < g.nx; x++)
-                {
-                    // FDK计算预留
-                }
-            }
-        }
-    }
+
 }
--- ReconCPU/cpu_katsevich.cpp
+++ ReconCPU/cpu_katsevich.cpp
@@ -18,23 +18,4 @@ void CpuKatsevichRecon::FilterProj(const std::vector<float>& in, std::vector<flo
 
 void CpuKatsevichRecon::BackProjectOMP(const std::vector<float>& proj, std::vector<float>& vol)
 {
-    const auto& g = m_geo;
-#pragma omp parallel
-    {
-        int tid = omp_get_thread_num();
-        int thr = omp_get_num_threads();
-        int zblk = g.nz / thr;
-        int zs = tid * zblk;
-        int ze = (tid == thr - 1) ? g.nz : (tid + 1) * zblk;
-        for (int z = zs; z < ze; z++)
-        {
-            for (int y = 0; y < g.ny; y++)
-            {
-                for (int x = 0; x < g.nx; x++)
-                {
-                    // Katsevich反投影预留
-                }
-            }
-        }
-    }
 }
